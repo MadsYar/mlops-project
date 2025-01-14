c08fcf7bc6ce983e69c3ebdb00f9c35ccb939f42
Jacopo Ceccuti <jacopo.ceccuti@gmail.com>
2025-01-14 13:17:24 +0100

    skip downloading data if not present

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,10 @@
 from danish_to_english_llm.data import TranslationDataset
 from transformers import T5TokenizerFast
 from torch.utils.data import Dataset
+import os
+import pytest
 
+@pytest.mark.skipif(not os.path.exists('data/raw'), reason="Data files not found")
 def test_translation_dataset():
     """Test the TranslationDataset class."""
     tokenizer = T5TokenizerFast.from_pretrained("google-t5/t5-small")
